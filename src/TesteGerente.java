ab21f0359e6a54c161fcb94437b293fccab61d15
Francisco Junior <franciscojdsjr@gmail.com>
2021-03-23 18:33:09 -0300

    aula2

---
+++ src/TesteGerente.java
@@ -0,0 +1,22 @@
+public class TesteGerente {
+
+    public static void main(String[] args) {
+
+        gerente g1 = new gerente();
+
+        g1.setNome("Francisco");
+        g1.setCpf("01327740460");
+        g1.setSalario(5500);
+        g1.setSenha(2222);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getCpf());
+        System.out.println(g1.getSalario());
+
+        boolean autenticou = g1.autentica(2222);
+
+        System.out.println(autenticou);
+
+    }
+
+}
---
+++ src/gerente.java
@@ -0,0 +1,24 @@
+//Gerente herda da classe funcionario
+public class gerente extends funcionario {
+
+    private int senha;
+
+    public boolean autentica(int senha) {
+
+        if (this.senha == senha) {
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+}
