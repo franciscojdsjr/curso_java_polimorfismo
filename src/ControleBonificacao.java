df92c1c20cab03a8931b61c689072b64cce1755a
Francisco Junior <franciscojdsjr@gmail.com>
2021-03-25 21:54:56 -0300

    aula5

--- src/ControleBonificacao.java
+++ src/ControleBonificacao.java
@@ -1,15 +1,15 @@
+
 public class ControleBonificacao {
-
-    private double soma;
-
-    public void registra(funcionario f) {
-
-        double bonificacao = f.getBonificacao();
-        this.soma = this.soma + bonificacao;
-    }
-
-    public double getSoma() {
-        return soma;
-    }
+	
+	private double soma;
+	
+	public void registra(Funcionario f) {
+		double boni = f.getBonificacao();
+		this.soma = this.soma + boni;
+	}
+	
+	public double getSoma() {
+		return soma;
+	}
 
 }
--- src/TesteReferencias.java
+++ src/TesteReferencias.java
@@ -1,25 +1,25 @@
+
 public class TesteReferencias {
 
-    public static void main(String[] args) {
-
-        gerente g1 = new gerente();
-        g1.setNome("Francisco");
-        g1.setSalario(5000);
-
-        funcionario f = new funcionario();
-        f.setSalario(2000);
-
-        EditorVideo ev = new EditorVideo();
-        ev.setSalario(2500);
-
-        ControleBonificacao controle = new ControleBonificacao();
-
-        controle.registra(g1);
-        controle.registra(f);
-        controle.registra(ev);
-
-        System.out.println(controle.getSoma());
-
-    }
+	public static void main(String[] args) {
+		
+		Gerente g1 = new Gerente();
+		g1.setNome("Marcos");
+		g1.setSalario(5000.0);
+	
+		EditorVideo ev = new EditorVideo();
+		ev.setSalario(2500.0);
+		
+		Designer d = new Designer();
+		d.setSalario(2000.0);
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(ev);
+		controle.registra(d);
+		
+		System.out.println(controle.getSoma());
+		
+	}
 
 }
